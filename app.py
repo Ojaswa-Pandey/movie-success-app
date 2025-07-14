add10034ce89917ef68d37403c091a3da51acea1
Ojaswa-Pandey <ojaswa16pandey@gmail.com>
2025-07-14 10:15:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ def predict():
     prediction = model.predict(input_data)[0]
 
     return render_template("result.html", prediction=prediction, cast_score=round(cast_score, 2))
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
